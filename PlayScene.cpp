4a5a42fe301f038d6b3e64271dd8d75717a4a465
gai <115063721+gai1637@users.noreply.github.com>
2023-11-02 15:51:35 +0900

    関数追加

--- PlayScene.cpp
+++ PlayScene.cpp
@@ -1,1 +1,16 @@
 #include "PlayScene.h"
+void (PlayScene::*PlayScene::pFuncTable[])() = {
+	&PlayScene::Title,
+	&PlayScene::Select,
+	&PlayScene::Game
+};
+//アップデート、ここにはこれ以外書かないこと
+void PlayScene::Update() { 
+	(this->*pFuncTable[static_cast<size_t>(scene)])(); 
+}
+//タイトル処理
+void PlayScene::Title() {}
+//ステージセレクトの処理
+void PlayScene::Select() {}
+//ゲーム本体の処理
+void PlayScene::Game() {}
--- PlayScene.h
+++ PlayScene.h
@@ -15,5 +15,33 @@ enum class Scene {
 class PlayScene {
 private:
 	Scene scene = Scene::Title;
+	static void (PlayScene::*pFuncTable[])();
 
+public:
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 毎フレーム処理
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+	/// <summary>
+	/// タイトル
+	/// </summary>
+	void Title();
+	/// <summary>
+	/// ステージ選択
+	/// </summary>
+	void Select();
+	/// <summary>
+	/// ゲーム
+	/// </summary>
+	void Game();
 };
